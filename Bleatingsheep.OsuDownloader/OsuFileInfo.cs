b8c8cf5ce264677c1fa8b8c28251b1293904eb6c
bleatingsheep <bleatingsheep@live.cn>
2019-03-01 20:39:58 +0800

    添加项目文件。

---
+++ Bleatingsheep.OsuDownloader/OsuFileInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Bleatingsheep.OsuDownloader
+{
+    public sealed class OsuFileInfo
+    {
+        [JsonProperty("file_version")]
+        public long FileVersion { get; set; }
+
+        [JsonProperty("filename")]
+        public string Filename { get; set; }
+
+        [JsonProperty("file_hash")]
+        public string FileHash { get; set; }
+
+        [JsonProperty("filesize")]
+        public long Filesize { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty("patch_id")]
+        public long? PatchId { get; set; }
+
+        [JsonProperty("url_full")]
+        public Uri UrlFull { get; set; }
+
+        [JsonProperty("url_patch", NullValueHandling = NullValueHandling.Ignore)]
+        public Uri UrlPatch { get; set; }
+    }
+}
---
+++ Bleatingsheep.OsuDownloader/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+
+namespace Bleatingsheep.OsuDownloader
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            byte[] StringToByteArray(string hex)
+            {
+                return Enumerable.Range(0, hex.Length)
+                                 .Where(x => x % 2 == 0)
+                                 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                                 .ToArray();
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                var infoJson = await httpClient.GetStringAsync(@"https://osu.ppy.sh/web/check-updates.php?action=check&stream=stable40");
+                var osuFileInfo = JsonConvert.DeserializeObject<OsuFileInfo[]>(infoJson);
+                var tempFile = Path.GetTempFileName();
+                var destFile = args.Length == 0 || string.IsNullOrEmpty(args[0])
+                    ? "osu!.zip"
+                    : args[0];
+
+                using (var fs = File.Create(tempFile))
+                using (var archive = new ZipArchive(fs, ZipArchiveMode.Create))
+                using (MD5 md5 = new MD5CryptoServiceProvider())
+                {
+                    foreach (var file in osuFileInfo)
+                    {
+                        ZipArchiveEntry fileEntry = archive.CreateEntry(file.Filename);
+                        fileEntry.LastWriteTime = file.Timestamp;
+                        using (Stream destination = fileEntry.Open())
+                        {
+                            var uriBuilder = new UriBuilder(file.UrlFull);
+                            uriBuilder.Scheme = "https";
+                            uriBuilder.Port = 443;
+                            var uri = uriBuilder.Uri;
+
+                            byte[] bytes = await httpClient.GetByteArrayAsync(uri);
+                            var actualHash = md5.ComputeHash(bytes);
+                            var expectedHash = StringToByteArray(file.FileHash);
+                            if (!expectedHash.SequenceEqual(actualHash))
+                                throw new HttpRequestException();
+                            await destination.WriteAsync(bytes);
+                        }
+                    }
+                }
+
+                File.Copy(tempFile, destFile, true);
+
+                try
+                {
+                    File.Delete(tempFile);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+}
